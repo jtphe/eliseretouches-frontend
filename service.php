86fba5b8126f85c5d86ab8485fa0d3c7245ff045
Jean <jean.phe.fr@gmail.com>
2020-05-07 09:37:03 +0200

    add code base

---
+++ service.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Nos prestations</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Nos prestations">
+    <meta name="author" content="Jean-Thavorak Phe">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/page_content.css" rel="stylesheet">
+    <link rel="apple-touch-icon" sizes="57x57" href="media/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="media/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="media/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="media/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="media/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="media/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="media/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="media/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="media/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="media/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="media/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="media/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="media/favicon/favicon-16x16.png">
+    <link rel="manifest" href="media/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/921fec88a5.js" crossorigin="anonymous"></script>
+    <!-- Matomo -->
+    <script type="text/javascript">
+        var _paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+            var u = "//jtphe.ddns.net/analytics/";
+            _paq.push(['setTrackerUrl', u + 'matomo.php']);
+            _paq.push(['setSiteId', '2']);
+            var d = document,
+                g = d.createElement('script'),
+                s = d.getElementsByTagName('script')[0];
+            g.type = 'text/javascript';
+            g.async = true;
+            g.defer = true;
+            g.src = u + 'matomo.js';
+            s.parentNode.insertBefore(g, s);
+        })();
+    </script>
+    <!-- End Matomo Code -->
+</head>
+
+<body class="body-erh">
+    <?php include("header.html") ?>
+    <div class="welcome-intro">
+        <h1 class="text-center welcome-intro-text">Nos prestations</h1>
+    </div>
+    <div class="section-price">
+        <div class="text-center">
+            <p class="more-details">Pour plus de détails sur une retouche précise</p>
+            <button class="btn btn-light"><a href="http://eliseretouches.com/contactus.php" class="button-more-details" style="text-decoration: none;">Contactez-moi</a></button>
+        </div>
+
+        <!--Prix pantalons-->
+        <div>
+            <table class="table table-price">
+                <tr>
+                    <td class="title-price">Pantalons</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Ourlet classique</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Ourlet invisible main</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Ourlet revers, fente</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Ourlet invisible avec talonette</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Ourlet revers avec talonette</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Changer de fermeture éclaire</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Changer élastique taille</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Reprendre ou augmenter la taille</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Réparation de jeans</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Changer une poche</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Rétrécir le bas</td>
+                </tr>
+            </table>
+        </div>
+
+        <!--Prix robes et jupes-->
+        <div>
+            <table class="table table-price">
+                <tr>
+                    <td class="title-price text-left">Jupes & robes</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Raccourcir le bas</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Reprendre la taille</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Changer la doublure</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Changer la fermeture éclaire jupe</td>
+                </tr>
+            </table>
+        </div>
+
+        <!--Prix chemises et tee-shirt-->
+        <div>
+            <table class="table table-price">
+                <tr>
+                    <td class="title-price">Chemises et tee-shirt</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Ourlet manches</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Ourlet bas</td>
+                </tr>
+            </table>
+        </div>
+
+        <!--Prix vestes et manteaux-->
+        <div>
+            <table class="table table-price">
+                <tr>
+                    <td class="title-price">Vestes & manteaux</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Cintrer veste et manteau femme</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Changer une poche</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Raccourcir les manches</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Raccourcir le bas</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Changer la fermeture éclaire</td>
+
+                </tr>
+            </table>
+        </div>
+
+        <!--Prix divers-->
+        <div>
+            <table class="table table-price">
+                <tr>
+                    <td class="title-price">Divers</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Couture décousue</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Racommodage</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Ourlet nappe, rideau</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Coussin</td>
+                </tr>
+                <tr>
+                <tr>
+                    <td class="text-left">Boutonnière main</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Pose de bouton</td>
+                </tr>
+                <tr>
+                    <td class="text-left">Pose de bouton de jeans</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+    <?php include("footer.html") ?>
+</body>
